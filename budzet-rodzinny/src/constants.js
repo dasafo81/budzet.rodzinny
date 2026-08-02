20278b6da067c15bc091279123061aff556705f3
Budget App Assistant <budzet-rodzinny@app.local>
2026-08-02 20:05:31 +0000

    Usuń Czynsz z kategorii DOM

--- budzet-rodzinny/src/constants.js
+++ budzet-rodzinny/src/constants.js
@@ -14,7 +14,7 @@ export const CAT_ICONS = {
 }
 
 export const CATEGORIES = {
-  DOM:          ['Prąd','Gaz','Woda','Wywóz śmieci','Internet','Ubezpieczenie','Podatek','Czynsz','Remonty','Ogród'],
+  DOM:          ['Prąd','Gaz','Woda','Wywóz śmieci','Internet','Ubezpieczenie','Podatek','Remonty','Ogród'],
   DZIECI:       ['Montessori','Angielski Benio','Same Judo','Talent','Wycieczki','Korepetycje','Pozostałe'],
   JEDZENIE:     ['Sklep spożywczy','Restauracja'],
   TRANSPORT:    ['Paliwo','Komunikacja miejska','Taksówka/Uber','Serwis auta'],
